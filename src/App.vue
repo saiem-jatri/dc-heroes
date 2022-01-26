d541030fde1a196faa1100c9a0c1d46a4692f952
saiemshahed <81529449+saiemshahed@users.noreply.github.com>
2022-01-26 17:44:02 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <h1>Hello vue {{ totalMOvies }}</h1>
+  <h2>{{fName}} {{lName}}</h2>
+  <ul>
+    <li v-for="(hero,index) in dcHeros" :key="hero.index">
+      <div>
+        {{ index }} : {{ hero.name }} <button @click="remove(index)">x</button>
+      </div>
+    </li>
+  </ul>
+
+  <form @submit.prevent="addMovie">
+    <input type="text" v-model.trim="newHero">
+    <button type="submit">ADD Hero</button>
+  </form>
+  <small>There are {{ herosCount }}</small>
+</template>
+
+<script>
+export default {
+  computed: {
+    herosCount() {
+      return this.dcHeros.length + " " + "movies"
+    },
+  },
+  methods: {
+    addMovie() {
+      if (this.newHero !== "") {
+
+        this.dcHeros.push({name: this.newHero});
+        this.newHero = ''
+      }
+    },
+    // remove(index){
+    //   (this.dcHeros.filter(hero,index)=>index !== index);
+    // },
+  },
+
+  data() {
+    return {
+      dcHeros: [
+        { name: "SuperGirl" },
+        { name: "Flash" },
+        { name: "Batman" },
+        { name: "Arrow" },
+        { name: "SuperMan" },
+      ],
+
+      newHero: "",
+
+      // attribute:"value"
+    };
+  }
+
+
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import demo from './App.vue'
+
+
+createApp(demo).mount('#app')
